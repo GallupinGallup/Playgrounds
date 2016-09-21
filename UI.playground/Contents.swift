18344b90d36c729fbb273f65e3a9d20e900896c1
GallupinGallup <gallupingallup@gmail.com>
2016-09-21 11:46:32 -0600

    Fixing stuff

---
+++ UI.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+var DynamicView=UIView(fraime:
+    CGRectMake(100, 200, 100, 100))
+DynamicView.backgroundDolor=UIColor.
+    GreenColor()
+DynamicView.layer.cornerRadius=25
+DynamicView.layer.borderWidth=2
+DynamicView
