1b8fc48bd7e021c9d5d6ff7f5127d8898e2b3048
GallupinGallup <gallupingallup@gmail.com>
2016-09-28 13:29:00 -0600

    I made labels and views

--- UI.playground/Contents.swift
+++ UI.playground/Contents.swift
@@ -4,18 +4,42 @@ import UIKit
 
 var DopeButton=UIButton(frame:CGRectMake(100,200,100,100))
 DopeButton.backgroundColor=UIColor.greenColor()
-
 DopeButton.layer.cornerRadius=100
 DopeButton.layer.borderWidth=5
 DopeButton
-
-
-var stuff = "Stuff is happening"
 
 var Button2=UIButton(frame:CGRectMake(200,200,200,200))
 Button2.backgroundColor=UIColor.purpleColor()
 Button2.layer.cornerRadius=325
 Button2.layer.borderWidth=7
 Button2
-var View1=UIView(frame:CGRect
 
+var View1=UIView(frame:CGRect(x: 150,y: 150,width: 150,height: 150))
+View1.backgroundColor=UIColor.orangeColor()
+View1.layer.cornerRadius=325
+View1.layer.borderWidth=20
+View1
+
+var View2=UIView(frame:CGRect(x: 150,y: 150,width: 150,height: 150))
+View2.backgroundColor=UIColor.blueColor()
+View2.layer.cornerRadius=275
+View2.layer.borderWidth=15
+View2
+
+var View3=UIView(frame:CGRect(x: 150,y: 150,width: 150,height: 150))
+View3.backgroundColor=UIColor.orangeColor()
+View3.layer.cornerRadius=250
+View3.layer.borderWidth=15
+View3
+
+var Label1 = UILabel (frame: CGRect(x: 175,y: 175,width: 175,height: 175))
+Label1.text = "This is a lable"
+Label1.backgroundColor=UIColor.yellowColor()
+Label1.textAlignment=NSTextAlignment.Center
+Label1
+
+var Label2 = UILabel (frame: CGRect(x: 400,y: 400,width: 400,height: 400))
+Label2.text = "I have made another lable"
+Label2.backgroundColor=UIColor.brownColor()
+Label2.textAlignment=NSTextAlignment.Right
+Label2
