7cae50e157d35a97709a83fc4dce2e456a58dcec
GallupinGallup <gallupingallup@gmail.com>
2016-09-28 12:49:59 -0600

    I have made a new playground for the UI Playgrounds canvas thing

--- UI.playground/Contents.swift
+++ UI.playground/Contents.swift
@@ -1,10 +1,21 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-var DynamicView=UIView(fraime:
-    CGRectMake(100, 200, 100, 100))
-DynamicView.backgroundDolor=UIColor.
-    GreenColor()
-DynamicView.layer.cornerRadius=25
-DynamicView.layer.borderWidth=2
-DynamicView
+
+var DopeButton=UIButton(frame:CGRectMake(100,200,100,100))
+DopeButton.backgroundColor=UIColor.greenColor()
+
+DopeButton.layer.cornerRadius=100
+DopeButton.layer.borderWidth=5
+DopeButton
+
+
+var stuff = "Stuff is happening"
+
+var Button2=UIButton(frame:CGRectMake(200,200,200,200))
+Button2.backgroundColor=UIColor.purpleColor()
+Button2.layer.cornerRadius=325
+Button2.layer.borderWidth=7
+Button2
+var View1=UIView(frame:CGRect
+
